7c847dfb999a897cf8d4d69dec822158ab3ad41f
Ishmael Hike <ishmaeliabaya@gmail.com>
2025-05-16 08:52:32 +0300

    project week 8

---
+++ script.js
@@ -0,0 +1,5 @@
+// This file contains the JavaScript code for the web application.
+// It adds interactivity to the webpage, such as handling user events and manipulating the DOM.
+function showMessage() {
+  alert("Thanks for clicking the button!");
+}
